1a8105d985530c6bacee550cd1b05efb9dd21788
Marc-Antoine <97919814+Marc2097@users.noreply.github.com>
2023-11-05 09:27:15 -0500

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,9 +1,48 @@
-from flask import Flask, render_template
-app = Flask(__name__, template_folder='../views', static_folder='../views/public')
+#
+# example-hologram-cloud-periodic-send.py - Example for sending periodic messages
+#                                           to the Hologram cloud.
+# Author: Hologram <support@hologram.io>
+#
+# Copyright 2016 - Hologram (Konekt, Inc.)
+#
+# LICENSE: Distributed under the terms of the MIT License
+#
 
-@app.route('/')
-def hello_world():
-    return render_template('index.html')
+import sys
+import time
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+sys.path.append(".")
+sys.path.append("..")
+sys.path.append("../..")
+
+from Hologram.HologramCloud import HologramCloud
+
+if __name__ == "__main__":
+    print("")
+    print("")
+    print("Testing Hologram Cloud class...")
+    print("")
+    print("* Note: You can obtain device keys from the Devices page")
+    print("* at https://dashboard.hologram.io")
+    print("")
+
+    device_key = input("What is your device key? ")
+
+    credentials = {'devicekey': device_key}
+
+    hologram = HologramCloud(credentials, authentication_type='csrpsk')
+
+    print('Sending a periodic message every 30 seconds...')
+    recv = hologram.sendPeriodicMessage(30, 'This is a periodic message',
+                                        topics=['PERIODICMESSAGES'],
+                                        timeout=6)
+
+    print('sleeping for 40 seconds...')
+    time.sleep(40)
+    print('waking up!')
+
+    hologram.stopPeriodicMessage()
+
+    print('')
+    print('Testing complete.')
+    print('')
